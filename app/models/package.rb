913f78919b4fb0612a5971da4e71b255bca79e1c
Gilang R. Aprianto <gizipp.gizipp@gmail.com>
2021-08-09 22:29:18 +0700

    Find package by budget.

--- app/models/package.rb
+++ app/models/package.rb
@@ -3,36 +3,42 @@ class Package < ActiveHash::Base
     {
       id: 1,
       name: "1-$1,000",
+      price_range: 1..1_000,
       price_text: "$39.99",
       type: "main"
     },
     {
       id: 2,
       name: "$1,000-$2,500",
+      price_range: 1_000..2_500,
       price_text: "$49.99",
       type: "main"
     },
     {
       id: 3,
       name: "$2,500-$5,000",
+      price_range: 2_500..5_000,
       price_text: "$59.99",
       type: "main"
     },
     {
       id: 4,
       name: "$5,000-$15,000",
+      price_range: 5_000..15_000,
       price_text: "$69.99",
       type: "main"
     },
     {
       id: 5,
       name: "$15,000-$30,000",
+      price_range: 15_000..30_000,
       price_text: "$79.99",
       type: "main"
     },
     {
       id: 6,
       name: "$30,000-$50,000",
+      price_range: 30_000..50_000,
       price_text: "$89.99",
       type: "main"
     },
@@ -62,6 +68,12 @@ class Package < ActiveHash::Base
 
   def self.addons
     where(type: "addons")
+  end
+
+  def self.find_package(price)
+    Package.main.each do |package|
+      return package if package.price_range.include?(price.to_i)
+    end
   end
 
   def formatted
